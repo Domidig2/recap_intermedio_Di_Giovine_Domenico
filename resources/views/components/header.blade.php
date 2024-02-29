00ded25e81ab3cd3721688fc9c1d20949d3a15f9
Domenico Di Giovine <domi.ddg97@gmail.com>
2024-02-29 17:04:14 +0100

    Fine settaggi iniziali

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="row justify-content-center ">
+        <div class="col-12 d-flex justify-content-center bg-success ">
+
+            <h1 class="display-1 my-5">{{$title}}</h1>
+
+        </div>
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+
+<x-header title="Welcome"/>
+
+</x-layout>
